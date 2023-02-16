46349112ca0ffcae34830b301b71866c55b9f723
Oscar Estudillo <oscar.estudillo@waapiti.eu>
2023-02-16 08:54:49 +0100

    Clean up files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HelloWorldMain from "./pages/hello-world/HelloWorldMain";
 import CustomMain from "./pages/custom-test/CustomMain";
@@ -6,16 +5,14 @@ import Index from "./pages/index";
 
 function App() {
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Index />} />
-          <Route path='*' element={<Index />} />
-          <Route path='/hello-world' element={<HelloWorldMain />} />
-          <Route path='/custom-test' element={<CustomMain />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Index />} />
+        <Route path='*' element={<Index />} />
+        <Route path='/hello-world' element={<HelloWorldMain />} />
+        <Route path='/custom-test' element={<CustomMain />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
